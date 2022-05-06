4e6c7c7526a0a846bbb5e4065acb634e6d34707d
TuanNDA <tuannda@outlook.in>
2022-05-06 08:19:45 +0700

    .

---
+++ packages/forms/src/Components/Concerns/HasExtraAlpineAttributes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Filament\Forms\Components\Concerns;
+
+use Filament\Support\Concerns\HasExtraAlpineAttributes as BaseTrait;
+
+/** @deprecated Use `\Filament\Support\Concerns\HasExtraAlpineAttributes` instead. */
+trait HasExtraAlpineAttributes
+{
+    use BaseTrait;
+}
---
+++ packages/forms/src/Components/Concerns/HasExtraAttributes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Filament\Forms\Components\Concerns;
+
+use Filament\Support\Concerns\HasExtraAttributes as BaseTrait;
+
+/** @deprecated Use `\Filament\Support\Concerns\HasExtraAttributes` instead. */
+trait HasExtraAttributes
+{
+    use BaseTrait;
+}
